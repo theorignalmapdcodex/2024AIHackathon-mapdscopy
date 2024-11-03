89addbe0a7c2ba9299d103c4b5f806c243c68e57
Shaunak Badani <shaunak.badani@duke.edu>
2024-11-02 20:09:00 -0400

    Updated dockerfile and incorporated prompt request

--- flashback/flask/llm_interface.py
+++ flashback/flask/llm_interface.py
@@ -1,4 +1,5 @@
-
+import requests
 
 def get_response_for_prompt(prompt):
-    return "Dummy response for prompt!"
+    response = requests.get("http://xiaoquankong.ai:10990/query", params = {"query_param" : prompt})
+    return response.json()['response']
